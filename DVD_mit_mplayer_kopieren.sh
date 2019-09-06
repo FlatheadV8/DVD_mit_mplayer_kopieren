80abf6fa84590912d4e14ffd1eb4ad928839414f
FlatheadV8 <FlatheadV8@users.noreply.github.com>
2019-09-06 13:08:41 +0200

    Update DVD_mit_mplayer_kopieren.sh
    Schönheitsveränderungen

--- DVD_mit_mplayer_kopieren.sh
+++ DVD_mit_mplayer_kopieren.sh
@@ -16,20 +16,20 @@
 #
 #------------------------------------------------------------------------------#
 
-VERSION="v2019090600"
+VERSION="v2019090601"
 
 ### FreeBSD
-#DVD="/dev/cd0"
-#DVD="/dev/$(camcontrol devlist | fgrep DVD | awk '{print $NF}' | tr -s '[(,)]' '\n' | grep -Ev '^$|pass[0-9]')"
+#DVD_DEV="/dev/cd0"
+#DVD_DEV="/dev/$(camcontrol devlist | grep -F DVD | awk '{print $NF}' | tr -s '[(,)]' '\n' | grep -Ev '^$|pass[0-9]')"
 #
 ### Linux
-#DVD="/dev/sr0"
-#DVD="/dev/$(grep -F DVD /sys/block/*/device/model | awk -F'/' '{print $4}')"
+#DVD_DEV="/dev/sr0"
+#DVD_DEV="/dev/$(grep -F DVD /sys/block/*/device/model | awk -F'/' '{print $4}')"
 #
 ### sollte in FreeBSD _und_ in Linux funktionieren
-DVD="$(ls -1 /dev/dvd* /dev/cd* 2>/dev/null | head -n1)"
+DVD_DEV="$(ls -1 /dev/dvd* /dev/cd* 2>/dev/null | head -n1)"
 
-DVD_IDENTIFY="$(mplayer dvd:// -dvd-device ${DVD} -vo null -ao null -identify -frames 0 -novideo -nosound -nocache 2>/dev/null)"
+DVD_IDENTIFY="$(mplayer dvd:// -dvd-device ${DVD_DEV} -vo null -ao null -identify -frames 0 -novideo -nosound -nocache 2>/dev/null)"
 TITEL_NR="$(echo "${DVD_IDENTIFY}" | grep -E '^ID_DVD_TITLE_[0-9]+_LENGTH=' | tr -s '[_.=]' ' ' | awk '{print $6,$4}' | awk '{print $NF}')"
 DVD_TITEL="$(echo "${DVD_IDENTIFY}" | grep -E '^ID_DVD_VOLUME_ID=' | tr -s '["=]' ' ' | awk '{print $NF}' | tr -s '[ .]' '_')"
 echo "1. DVD_TITEL='${DVD_TITEL}'"
@@ -48,11 +48,11 @@ cd "${DVD_TITEL}" || exit
 
 time for T_NR in ${TITEL_NR}
 do
-        # mplayer -dvd-device /dev/${DVDGERAET} dvd://0 -dumpstream -dumpfile title0.mpg
+        # mplayer dvd://0 -dvd-device ${DVD_DEV} -dumpstream -dumpfile title0.mpg
         echo "mplayer dvd://${T_NR} -mc 0 -forceidx -aspect 16:9 -dumpstream -dumpfile ${DVD_TITEL}_${T_NR}.mpg"
         mplayer dvd://${T_NR} -mc 0 -forceidx -aspect 16:9 -dumpstream -dumpfile ${DVD_TITEL}_${T_NR}.mpg
 done
 
 cd -
 
-eject ${DVD}
+eject ${DVD_DEV}
